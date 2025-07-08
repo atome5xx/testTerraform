fac73f15da301b690e993ad5d26f318cdcde7f5b
thomas <tletoubgym74@gmail.com>
2025-07-08 18:10:25 +0200

    nouveau push

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('App déployée avec succès !');
+    res.send('App déployée avec succès ! Et j\'ai push');
 });
 
 const PORT = process.env.PORT || 3000;
